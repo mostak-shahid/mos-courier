8d513fe2b9ef44f7d283d228302f84a359109956
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-01-10 18:12:37 +0600

    update

--- js/mos-courier-ajax.js
+++ js/mos-courier-ajax.js
@@ -1,4 +1,27 @@
 jQuery(document).ready(function($) {
+
+    $('.track-form').submit(function(e){
+        e.preventDefault();
+        var form_data = $(this).serialize();
+        console.log(form_data);
+        $.ajax({
+            url: ajax_obj.ajax_url, // or example_ajax_obj.ajaxurl if using on frontend
+            type:"POST",
+            dataType:"json",
+            data: {
+                'action': 'order_tracking',
+                'form_data' : form_data,
+            },
+            success: function(result){
+                // console.log(result);
+                $('.track-output').html(result);
+            },
+            error: function(errorThrown){
+                console.log(errorThrown);
+            }
+        });
+    });
+
     $('#action_order_form').submit(function(e){
         var data = $('#order_table_action').val();
         if (data == 'Delete'){
--- mos-courier-hooks.php
+++ mos-courier-hooks.php
@@ -3960,3 +3960,57 @@ function your_name_integrateWithVC() {
 	));
 }
 /*VC*/
+/*Ajax*/
+function order_tracking_func() {
+	$output = array();
+	$html = '<div class="w-100 pt-4"><div class="mt-2 pt-4 border-top"><div class="pt-2">';
+	$data = array();
+    if ( isset($_REQUEST) ) {
+		$value = explode('=', $_REQUEST['form_data']);
+		$data[$value[0]] = $value[1]; 
+		$order = get_page_by_title( $value[1], OBJECT,'courierorder' );
+
+		$args = array(
+			'post_type' => 'courierorder',
+			'posts_per_page' => 1,
+			'meta_key'  => '_mos_courier_receiver_number',
+			'meta_value'  => $value[1],
+		);
+		$query = new WP_Query( $args );
+
+		if ($order) {
+			$html .= '<table class="table table-borderless table-sm">';
+			$html .= '<tr><th>CL No</th><td class="text-right">'.$value[1].'</td></tr>';
+			$html .= '<tr><th>Mechant Order ID</th><td class="text-right">'.$order->ID.'</td></tr>';
+			$html .= '<tr><th>Zone</th><td class="text-right">'.get_post_meta( $order->ID, '_mos_courier_delivery_zone', true ).'</td></tr>';
+			$html .= '<tr><th>Booking Date</th><td class="text-right">'.get_post_meta( $order->ID, '_mos_courier_booking_date', true ).'</td></tr>';
+			$html .= '<tr><th>Delivery Date</th><td class="text-right">'.get_post_meta( $order->ID, '_mos_courier_delivery_date', true ).'</td></tr>';
+			$html .= '<tr><th>Current Status</th><td class="text-right">'.get_post_meta( $order->ID, '_mos_courier_delivery_status', true ).'</td></tr>';
+			$html .= '<tr><th>Receiver Name</th><td class="text-right">'.get_post_meta( $order->ID, '_mos_courier_receiver_name', true ).'</td></tr>';
+			$html .= '<tr><th>Remarks</th><td class="text-right">'.get_post_meta( $order->ID, '_mos_courier_remarks', true ).'</td></tr>';
+			$html .= '</table>';
+		} elseif ( $query->have_posts() ) {
+			while ( $query->have_posts() ) : $query->the_post();
+				$html .= '<table class="table table-borderless table-sm">';
+				$html .= '<tr><th>CL No</th><td class="text-right">'.get_the_title().'</td></tr>';
+				$html .= '<tr><th>Mechant Order ID</th><td class="text-right">'.get_the_ID().'</td></tr>';
+				$html .= '<tr><th>Zone</th><td class="text-right">'.get_post_meta( get_the_ID(), '_mos_courier_delivery_zone', true ).'</td></tr>';
+				$html .= '<tr><th>Booking Date</th><td class="text-right">'.get_post_meta( get_the_ID(), '_mos_courier_booking_date', true ).'</td></tr>';
+				$html .= '<tr><th>Delivery Date</th><td class="text-right">'.get_post_meta( get_the_ID(), '_mos_courier_delivery_date', true ).'</td></tr>';
+				$html .= '<tr><th>Current Status</th><td class="text-right">'.get_post_meta( get_the_ID(), '_mos_courier_delivery_status', true ).'</td></tr>';
+				$html .= '<tr><th>Receiver Name</th><td class="text-right">'.get_post_meta( get_the_ID(), '_mos_courier_receiver_name', true ).'</td></tr>';
+				$html .= '<tr><th>Remarks</th><td class="text-right">'.get_post_meta( get_the_ID(), '_mos_courier_remarks', true ).'</td></tr>';
+				$html .= '</table>';
+		    endwhile;			 
+		    wp_reset_postdata();
+		} else {
+			$html .= '<h5 id="no-order" class="text-color-2 text-center font-18 font-md-16 mb-0">Sorry! Found Nothing. </h5>';
+		}
+		$html .= '</div></div></div>'; 	
+		echo json_encode($html);   
+    }
+	die();
+} 
+add_action( 'wp_ajax_order_tracking', 'order_tracking_func' );
+add_action( 'wp_ajax_nopriv_order_tracking', 'order_tracking_func' );
+/*Ajax*/
