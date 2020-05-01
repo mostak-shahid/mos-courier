813e27a8e82f679bab9bbf789a3ecd38a0f34170
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-05-01 22:48:35 +0600

    Update mos-courier.php

--- mos-courier.php
+++ mos-courier.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Mos Courier
 Description: A Courier plugin by Md. Mostak Shahid.
-Version: 0.0.25
+Version: 0.0.26
 Author: Md. Mostak Shahid
 */
 require_once('plugins/update/plugin-update-checker.php');
