adb7489d26f499689222edfeb899141c0362f399
Md. Mostak Shahid <mostak.shahid@gmail.com>
2020-06-20 21:58:34 +0600

    Update mos-courier-functions.php

--- mos-courier-functions.php
+++ mos-courier-functions.php
@@ -1,7 +1,7 @@
 <?php
 function clean($string) {
    // $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-   $string = preg_replace('/[^A-Za-z0-9\-<h5></h5>]/', '', $string); // Removes special chars.
+   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
 
    return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
 }
