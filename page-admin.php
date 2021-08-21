9ea3d60a83411430b5fe08165b6bb44fbbcbacef
Md. Mostak Shahid <mostak.shahid@gmail.com>
2021-08-21 16:39:27 +0600

    Update page-admin.php

--- page-admin.php
+++ page-admin.php
@@ -1,6 +1,6 @@
 <?php
 global $page_list;
-$page_slug = (isset($_GET['p'])) ? $_GET['p'] : 'welcome';
+$page_slug = (isset($_GET['p'])) ? $_GET['p'] : 'dashboard';
 require_once ( plugin_dir_path( MOS_COURIER_FILE ) . 'header-courier.php' );
 ?>
 
